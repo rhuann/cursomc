90cdd3beb1b291206a95d314c0d85f492854179f
Rhuan Menezes <rhuann@gmail.com>
2020-08-12 14:41:33 -0400

    Criando repository e service para Categoria

---
+++ src/main/java/com/rhuan/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.rhuan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rhuan.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+
+}
