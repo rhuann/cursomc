5d916f4b29ceffa48d35e006c1824d3d86ddf8e3
Rhuan Menezes <rhuann@gmail.com>
2020-08-13 10:34:49 -0400

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/rhuan/cursomc/CursomcApplication.java
+++ src/main/java/com/rhuan/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.rhuan.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.rhuan.cursomc.domain.Cidade;
 import com.rhuan.cursomc.domain.Cliente;
 import com.rhuan.cursomc.domain.Endereco;
 import com.rhuan.cursomc.domain.Estado;
+import com.rhuan.cursomc.domain.Pagamento;
+import com.rhuan.cursomc.domain.PagamentoComBoleto;
+import com.rhuan.cursomc.domain.PagamentoComCartao;
+import com.rhuan.cursomc.domain.Pedido;
 import com.rhuan.cursomc.domain.Produto;
+import com.rhuan.cursomc.domain.enums.EstadoPagamento;
 import com.rhuan.cursomc.domain.enums.TipoCliente;
 import com.rhuan.cursomc.repositories.CategoriaRepository;
 import com.rhuan.cursomc.repositories.CidadeRepository;
 import com.rhuan.cursomc.repositories.ClienteRepository;
 import com.rhuan.cursomc.repositories.EnderecoRepository;
 import com.rhuan.cursomc.repositories.EstadoRepository;
+import com.rhuan.cursomc.repositories.PagamentoRepository;
+import com.rhuan.cursomc.repositories.PedidoRepository;
 import com.rhuan.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -26,21 +34,20 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
 	@Autowired
 	private ProdutoRepository produtoRepository;
-
 	@Autowired
 	private EstadoRepository estadoRepository;
-
 	@Autowired
 	private CidadeRepository cidadeRepository;
-
 	@Autowired
 	private ClienteRepository clienteRepository;
-
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,6 +96,23 @@ public class CursomcApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
 	}
 
