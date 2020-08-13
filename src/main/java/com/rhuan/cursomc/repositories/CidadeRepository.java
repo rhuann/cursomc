39780589645de819e5a3dfbbe0ffa37ba91c45a9
Rhuan Menezes <rhuann@gmail.com>
2020-08-13 08:47:41 -0400

    Estado e Cidade

---
+++ src/main/java/com/rhuan/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.rhuan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rhuan.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+
+}
