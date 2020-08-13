dc1a20bb45a1ec77c9f2ba5d1d4b0b8b5c10075b
Rhuan Menezes <rhuann@gmail.com>
2020-08-13 11:43:27 -0400

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/rhuan/cursomc/domain/Categoria.java
+++ src/main/java/com/rhuan/cursomc/domain/Categoria.java
@@ -10,21 +10,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
-	@JsonManagedReference
-	@ManyToMany(mappedBy="categorias")
+	@ManyToMany(mappedBy = "categorias")
 	List<Produto> produtos = new ArrayList<>();
-	
+
 	public Categoria() {
 	}
 
@@ -49,7 +46,7 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
--- src/main/java/com/rhuan/cursomc/domain/Produto.java
+++ src/main/java/com/rhuan/cursomc/domain/Produto.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -47,7 +46,6 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
-	
 	@JsonIgnore
 	public List<Pedido> getPedidos() {
 		List<Pedido> lista = new ArrayList<>();
