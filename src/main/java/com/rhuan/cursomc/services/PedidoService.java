55d36b04b6bb6e5810615f192c198032e59d3dce
Rhuan Menezes <rhuann@gmail.com>
2020-08-17 08:42:24 -0400

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/rhuan/cursomc/services/PedidoService.java
+++ src/main/java/com/rhuan/cursomc/services/PedidoService.java
@@ -14,10 +14,10 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
-	
-	public Pedido buscar(Integer id) {
+
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
-	}	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
 }
