87c839152a2e97954d811481cf89c4ad8ccef23e
Rhuan Menezes <rhuann@gmail.com>
2020-08-17 10:31:44 -0400

    Validação sintática com Bean Validation

--- src/main/java/com/rhuan/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/rhuan/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.rhuan.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.rhuan.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entra 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/com/rhuan/cursomc/services/CategoriaService.java
+++ src/main/java/com/rhuan/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rhuan.cursomc.domain.Categoria;
+import com.rhuan.cursomc.dto.CategoriaDTO;
 import com.rhuan.cursomc.repositories.CategoriaRepository;
 import com.rhuan.cursomc.services.exceptions.DataIntegrityException;
 import com.rhuan.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
