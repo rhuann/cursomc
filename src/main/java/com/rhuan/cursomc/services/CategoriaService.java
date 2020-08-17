e3cf2ed5071e3526685800a1c64221423901b95b
Rhuan Menezes <rhuann@gmail.com>
2020-08-17 08:31:42 -0400

    Inserindo uma nova Categoria com POST

--- src/main/java/com/rhuan/cursomc/services/CategoriaService.java
+++ src/main/java/com/rhuan/cursomc/services/CategoriaService.java
@@ -14,10 +14,15 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-	}	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);		
+		return repo.save(obj);
+	}
 }
