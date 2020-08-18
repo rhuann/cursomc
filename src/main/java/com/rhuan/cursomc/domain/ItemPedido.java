c353be425665057a282cd99d67cdf6cfafc8dded
Rhuan Menezes <rhuann@gmail.com>
2020-08-18 13:32:03 -0400

    Subtotal e total do Pedido

--- src/main/java/com/rhuan/cursomc/domain/ItemPedido.java
+++ src/main/java/com/rhuan/cursomc/domain/ItemPedido.java
@@ -17,19 +17,23 @@ public class ItemPedido implements Serializable {
 
 	private Double desconto;
 	private Integer quantidade;
-	private Double preço;
+	private Double preco;
 
 	public ItemPedido() {
 
 	}
 
-	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preço) {
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
 		id.setPedido(pedido);
 		id.setProduto(produto);
 		this.desconto = desconto;
 		this.quantidade = quantidade;
-		this.preço = preço;
+		this.preco = preco;
+	}
+
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
 	}
 
 	@JsonIgnore
@@ -65,12 +69,12 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 	}
 
-	public Double getPreço() {
-		return preço;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setPreço(Double preço) {
-		this.preço = preço;
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
 	@Override
